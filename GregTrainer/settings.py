ee9fba5ff9c278e7084751128c2bc807e851c6d8
efmarcial-dev <eduardo@novadev.solutions>
2025-04-10 14:27:14 -0500

    added host

--- GregTrainer/settings.py
+++ GregTrainer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cg8lq&dvegv$c_psergg8-iq2wqbaryqjbk4$#b$wtunho53&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trainer-cstd.onrender.com', '*']
 
 
 # Application definition
